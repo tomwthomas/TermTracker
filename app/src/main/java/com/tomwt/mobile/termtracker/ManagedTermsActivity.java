b9b19a12b8f973a7352164ef025e9c5e13954919
twt <tomwthomas@gmail.com>
2017-03-04 22:14:10 -0800

    Prepping to make data provider more generic

--- app/src/main/java/com/tomwt/mobile/termtracker/ManagedTermsActivity.java
+++ app/src/main/java/com/tomwt/mobile/termtracker/ManagedTermsActivity.java
@@ -1,14 +1,20 @@
 package com.tomwt.mobile.termtracker;
 
+import android.annotation.TargetApi;
+import android.database.Cursor;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.CursorAdapter;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ListView;
 
 public class ManagedTermsActivity extends AppCompatActivity {
-
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +31,18 @@ public class ManagedTermsActivity extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Manage Terms");
+
+        // build out the list of upcoming milestones and display them in the GUI
+        {
+            Cursor cursor = getContentResolver().query(TermTrackerProvider.CONTENT_URI, DBOpenHelper.NOTES_ALL_COLUMNS, null, null, null, null);
+            String[] from = {DBOpenHelper.NOTES_DETAILS};
+            int[] to = {android.R.id.text1};
+            CursorAdapter cursorAdapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_1, cursor, from, to, 0);
+
+            ListView list = (ListView) findViewById(android.R.id.list);
+            list.setAdapter(cursorAdapter);
+        }
     }
 
 }
